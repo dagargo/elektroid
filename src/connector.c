6885c861bc66f0ccdd18473eaeafef1f9386e7de
dagargo <dagargo@gmail.com>
2020-06-22 13:06:11 +0200

    Decrease data read timeout to 2 s

--- src/connector.c
+++ src/connector.c
@@ -28,7 +28,7 @@
 
 #define BUFF_SIZE 512
 #define TRANSF_BLOCK_SIZE 0x2000
-#define READ_TIMEOUT 5
+#define READ_TIMEOUT 2
 #define SLEEP_ENODATA 50
 
 static const guint8 MSG_HEADER[] = { 0xf0, 0, 0x20, 0x3c, 0x10, 0 };
