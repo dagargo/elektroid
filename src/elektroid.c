dagargo/elektroid

commit 7afbb896758ed151a9e1d32b51b769310f0d33e7

Start sample playback before its fully loaded

src/elektroid.c
@@ -531,9 +531,9 @@ elektroid_controls_set_sensitive (gboolean sensitive)
 }
 
 static gboolean
-elektroid_update_ui_after_load (gpointer data)
+elektroid_update_ui_on_load (gpointer data)
 {
-  if (audio.sample->len > 0)
+  if (audio.sample->len >= LOAD_BUFFER_LEN || !load_thread_running)
     {
       if (audio_check (&audio))
 	{
@@ -784,7 +784,6 @@ elektroid_load_sample (gpointer path)
   sample_load (audio.sample, &load_mutex, &audio.frames, path,
 	       &load_thread_running, elektroid_redraw_sample);
   free (path);
-  g_idle_add (elektroid_update_ui_after_load, NULL);
   return NULL;
 }
 
@@ -793,6 +792,7 @@ elektroid_start_load_thread (gchar * path)
 {
   debug_print (1, "Creating load thread...\n");
   load_thread_running = TRUE;
+  g_timeout_add (100, elektroid_update_ui_on_load, NULL);
   load_thread = g_thread_new ("load_sample", elektroid_load_sample, path);
 }
 

src/sample.c
@@ -25,8 +25,6 @@
 #include "sample.h"
 #include "utils.h"
 
-#define LOAD_BUFFER_LEN 10000
-
 size_t
 sample_save (GArray * sample, gchar * path)
 {
@@ -252,6 +250,8 @@ cleanup:
   free (src_data.data_out);
 
   sf_close (sndfile);
+
+  *running = FALSE;
 
   return sample->len;
 }

src/sample.h
@@ -22,6 +22,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#define LOAD_BUFFER_LEN 4800
+
 size_t sample_save (GArray *, gchar *);
 
 size_t sample_load (GArray *, GMutex *, gboolean *, gchar *, gint *,

commit 5fbfa816acd4e188d5b4c996a849ae76c6e250e9

Fix regression bug introduced in 7afbb89 that make upload transfers to be canceled

src/elektroid.c
@@ -783,6 +783,7 @@ elektroid_load_sample (gpointer path)
 {
   sample_load (audio.sample, &load_mutex, &audio.frames, path,
 	       &load_thread_running, elektroid_redraw_sample);
+  load_thread_running = FALSE;
   free (path);
   return NULL;
 }

src/sample.c
@@ -251,7 +251,5 @@ cleanup:
 
   sf_close (sndfile);
 
-  *running = FALSE;
-
   return sample->len;
 }