9ad35fe098a425e9fe5f8d9d4749b74d0c2214e1
dagargo <dagargo@gmail.com>
2021-01-05 14:59:58 +0100

    Include file, line number and function in log

--- src/utils.h
+++ src/utils.h
@@ -25,7 +25,7 @@
 #define ELEKTROID_FILE 'F'
 #define ELEKTROID_DIR 'D'
 
-#define debug_print(level, ...) if (level <= debug_level) fprintf(stderr, __VA_ARGS__)
+#define debug_print(level, format, ...) if (level <= debug_level) fprintf(stderr, __FILE__ ":%d:(%s):" format, __LINE__, __FUNCTION__, ## __VA_ARGS__)
 
 extern int debug_level;
 
