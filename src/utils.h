c03d9cf6f03a90a9dbda28cfebe128e40b9cd790
dagargo <dagargo@gmail.com>
2021-01-30 13:07:32 +0100

    Print error messages in red

--- src/utils.h
+++ src/utils.h
@@ -26,7 +26,7 @@
 #define ELEKTROID_DIR 'D'
 
 #define debug_print(level, format, ...) if (level <= debug_level) fprintf(stderr, "DEBUG:" __FILE__ ":%d:(%s): " format, __LINE__, __FUNCTION__, ## __VA_ARGS__)
-#define error_print(format, ...) fprintf(stderr, "ERROR:" __FILE__ ":%d:(%s): " format, __LINE__, __FUNCTION__, ## __VA_ARGS__)
+#define error_print(format, ...) fprintf(stderr, "\x1b[31mERROR:" __FILE__ ":%d:(%s): " format "\x1b[m", __LINE__, __FUNCTION__, ## __VA_ARGS__)
 
 extern int debug_level;
 
