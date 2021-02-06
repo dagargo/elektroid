e902480d4919179da3399380e9b49417df5f0af8
dagargo <dagargo@gmail.com>
2021-02-06 15:21:47 +0100

    Add pauses between calls to elektroid-cli in test

--- test/full_test.sh
+++ test/full_test.sh
@@ -9,19 +9,30 @@ DEVICE=$($ecli ld | head -n 1 | awk '{print $1}')
 [ -z "$DEVICE" ] && echo "No device found" && exit 1
 echo "Using device $DEVICE..."
 
+sleep 1
+
 echo "Testing ls..."
 $ecli ls $DEVICE:/
 [ $? -ne 0 ] && exit 1
 
+sleep 1
+
 echo "Testing mkdir..."
 $ecli mkdir $DEVICE:/$TEST_NAME
 [ $? -ne 0 ] && exit 1
+
+sleep 1
+
 $ecli ls $DEVICE:/$TEST_NAME
 [ $? -ne 0 ] && exit 1
+
+sleep 1
 
 echo "Testing upload..."
 $ecli upload $srcdir/res/square.wav $DEVICE:/$TEST_NAME
 [ $? -ne 0 ] && exit 1
+
+sleep 1
 
 output=$($ecli ls $DEVICE:/$TEST_NAME)
 echo $output
@@ -30,6 +41,8 @@ size=$(echo $output | awk '{print $2}')
 name=$(echo $output | awk '{print $4}')
 [ "$type" != "F" ] || [ "$size" != "0.09" ] || [ "$name" != "square" ] && exit 1
 
+sleep 1
+
 echo "Testing download..."
 $ecli download $DEVICE:/$TEST_NAME/square
 [ $? -ne 0 ] && exit 1
@@ -37,13 +50,19 @@ actual_cksum="$(cksum $srcdir/square.wav | awk '{print $1}')"
 rm $srcdir/square.wav
 [ "$actual_cksum" != "$(cksum $srcdir/res/square.wav | awk '{print $1}')" ] && exit 1
 
+sleep 1
+
 echo "Testing mv..."
 $ecli mv $DEVICE:/$TEST_NAME/square $DEVICE:/$TEST_NAME/sample
 [ $? -ne 0 ] && exit 1
 
+sleep 1
+
 echo "Testing rm..."
 $ecli rm $DEVICE:/$TEST_NAME/sample
 [ $? -ne 0 ] && exit 1
+
+sleep 1
 
 echo "Testing rmdir..."
 $ecli rmdir $DEVICE:/$TEST_NAME
