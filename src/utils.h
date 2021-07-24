56a8477de6e233561e8168634c9f1a5af53f995c
dagargo <dagargo@gmail.com>
2021-07-24 21:23:41 +0200

    Replace constants with enum

--- src/utils.h
+++ src/utils.h
@@ -25,12 +25,15 @@
 
 #define LABEL_MAX 128
 
-#define ELEKTROID_FILE 'F'
-#define ELEKTROID_DIR 'D'
-#define ELEKTROID_NONE '?'
-
 #define debug_print(level, format, ...) if (level <= debug_level) fprintf(stderr, "DEBUG:" __FILE__ ":%d:(%s): " format, __LINE__, __FUNCTION__, ## __VA_ARGS__)
 #define error_print(format, ...) fprintf(stderr, "\x1b[31mERROR:" __FILE__ ":%d:(%s): " format "\x1b[m", __LINE__, __FUNCTION__, ## __VA_ARGS__)
+
+enum item_type
+{
+  ELEKTROID_NONE = 0,
+  ELEKTROID_FILE = 'F',
+  ELEKTROID_DIR = 'D'
+};
 
 struct item
 {
