35549c23c1c07302235240ff7655107f02969773
dagargo <dagargo@gmail.com>
2021-08-14 15:09:17 +0200

    Add missing va_end call

--- src/elektroid.c
+++ src/elektroid.c
@@ -308,10 +308,12 @@ show_error_msg (const char *format, ...)
   dialog = gtk_message_dialog_new (GTK_WINDOW (main_window),
 				   GTK_DIALOG_DESTROY_WITH_PARENT |
 				   GTK_DIALOG_MODAL,
-				   GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, "%s", msg);
+				   GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, "%s",
+				   msg);
   gtk_dialog_run (GTK_DIALOG (dialog));
   gtk_widget_destroy (dialog);
   g_free (msg);
+  va_end (args);
 }
 
 static void
