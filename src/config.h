8c259c9933d4a22f385eefcca1580864d42d8999
dagargo <dagargo@gmail.com>
2021-10-31 11:15:31 +0100

    Add missing header file

---
+++ src/config.h
@@ -0,0 +1,33 @@
+/*
+ *   config.h
+ *   Copyright (C) 2019 David García Goñi <dagargo@gmail.com>
+ *
+ *   This file is part of Elektroid.
+ *
+ *   Elektroid is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Elektroid is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Elektroid. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+
+struct config
+{
+  gboolean autoplay;
+  gchar *local_dir;
+};
+
+gint config_save (struct config *);
+
+gint config_load (struct config *);
+
+void config_free (struct config *);
