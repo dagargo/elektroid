493f6e429232afefd60e640281d71ec99f90e40e
dagargo <dagargo@gmail.com>
2022-05-29 15:59:01 +0200

    Use color in error_print only if stderr is a terminal

--- src/utils.h
+++ src/utils.h
@@ -30,7 +30,7 @@
 #define ELEKTRON_SAMPLE_RATE 48000
 
 #define debug_print(level, format, ...) if (level <= debug_level) fprintf(stderr, "DEBUG:" __FILE__ ":%d:(%s): " format, __LINE__, __FUNCTION__, ## __VA_ARGS__)
-#define error_print(format, ...) fprintf(stderr, "\x1b[31mERROR:" __FILE__ ":%d:(%s): " format "\x1b[m", __LINE__, __FUNCTION__, ## __VA_ARGS__)
+#define error_print(format, ...) fprintf(stderr, "%sERROR:" __FILE__ ":%d:(%s): " format "%s", isatty(fileno(stderr)) ? "\x1b[31m" : "", __LINE__, __FUNCTION__, ## __VA_ARGS__, isatty(fileno(stderr)) ? "\x1b[m" : "")
 
 enum item_type
 {
