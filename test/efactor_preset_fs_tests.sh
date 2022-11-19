cc9701ba54319dc672ea143ee2b8dfb1d3ac6b0f
dagargo <dagargo@gmail.com>
2022-11-19 16:32:43 +0100

    Improve efactor tests

--- test/efactor_preset_fs_tests.sh
+++ test/efactor_preset_fs_tests.sh
@@ -1,34 +1,41 @@
 #!/usr/bin/env bash
 
+TEST_PRESET=99
+
+function exitWithError() {
+  echo "Restoring preset..."
+  $ecli efactor-preset-ul "$f" $TEST_DEVICE:/$TEST_PRESET
+  rm "$f"
+  exit $1
+}
+
 echo "Using device $TEST_DEVICE..."
 
 echo "Backing up preset..."
-$ecli efactor-preset-dl $TEST_DEVICE:/99
-f=Eventide\ Factor\ *
+$ecli efactor-preset-dl $TEST_DEVICE:/$TEST_PRESET
+f=$(echo Eventide\ Factor\ *)
 
 echo "Testing ls..."
 files=$($ecli efactor-preset-ls $TEST_DEVICE:/)
-[ $? -ne 0 ] && exit 1
+[ $? -ne 0 ] && exitWithError 1
 
 echo "Testing upload..."
-$ecli efactor-preset-ul "$srcdir/res/Eventide Factor Preset.syx" $TEST_DEVICE:/99
-[ $? -ne 0 ] && exit 1
+$ecli efactor-preset-ul "$srcdir/res/Eventide Factor Preset.syx" $TEST_DEVICE:/$TEST_PRESET
+[ $? -ne 0 ] && exitWithError 1
 
 #Uploading with name makes no sense for factor pedals as the file includes the name.
 
 echo "Testing mv..."
-$ecli efactor-preset-mv $TEST_DEVICE:/99 "New Name"
-[ $? -ne 0 ] && exit 1
+$ecli efactor-preset-mv $TEST_DEVICE:/$TEST_PRESET "New Name"
+[ $? -ne 0 ] && exitWithError 1
 
 echo "Testing download..."
-$ecli efactor-preset-dl $TEST_DEVICE:/99
-[ $? -ne 0 ] && exit 1
+$ecli efactor-preset-dl $TEST_DEVICE:/$TEST_PRESET
+[ $? -ne 0 ] && exitWithError 1
 actual_cksum="$(cksum "Eventide Factor New Name.syx" | awk '{print $1}')"
 rm "Eventide Factor New Name.syx"
-[ "$actual_cksum" != $(cksum "$srcdir/res/Eventide Factor New Name.syx" | awk '{print $1}') ] && exit 1
+echo $actual_cksum  | awk '{print $1}'
+cksum "$srcdir/res/Eventide Factor New Name.syx"
+[ "$actual_cksum" != $(cksum "$srcdir/res/Eventide Factor New Name.syx" | awk '{print $1}') ] && exitWithError 1
 
-echo "Restoring preset..."
-$ecli efactor-preset-ul $f $TEST_DEVICE:/99
-rm "$f"
-
-exit 0
+exitWithError 0
