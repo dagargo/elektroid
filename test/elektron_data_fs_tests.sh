bd20aa39aa9982cb1ba8fda96ce8c95db3fd2040
dagargo <dagargo@gmail.com>
2022-12-10 17:17:28 +0100

    Add sound backup to tests

--- test/elektron_data_fs_tests.sh
+++ test/elektron_data_fs_tests.sh
@@ -2,13 +2,14 @@
 
 export ELEKTROID_ELEKTRON_JSON=$srcdir/res/devices.json
 
-function cleanupAndExitWithError () {
-  $ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/1
-  $ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/62
-  $ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/63
-  $ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/64
-  $ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/256
-  exit 1
+function cleanupAndExit () {
+  for s in 1 62 63 64 256; do
+    if [ -f $s.dtdata.bak ]; then
+      $ecli elektron-data-ul $s.dtdata.bak $TEST_DEVICE:/soundbanks/H/$s
+      rm -f $s.dtdata.bak
+    fi
+  done
+  exit $1
 }
 
 function get_sound_n_with_id () {
@@ -18,6 +19,13 @@ function get_sound_n_with_id () {
 
 echo "Using device $TEST_DEVICE..."
 
+echo "Preparing tests..."
+for s in 1 62 63 64 256; do
+  $ecli elektron-data-dl $TEST_DEVICE:/soundbanks/H/$s
+  mv *.dtdata $s.dtdata.bak > /dev/null 2>&1
+done
+$ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/1
+
 sound1=$($ecli elektron-data-ls $TEST_DEVICE:/soundbanks/A | grep "^F   1")
 nsound1=$(get_sound_n_with_id 1 64)
 
@@ -25,74 +33,73 @@ sound2=$($ecli elektron-data-ls $TEST_DEVICE:/soundbanks/A | grep "^F   2")
 
 echo "Testing data copy..."
 $ecli elektron-data-cp $TEST_DEVICE:/soundbanks/A/1 $TEST_DEVICE:/soundbanks/H/64
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 $ecli elektron-data-cp $TEST_DEVICE:/soundbanks/A/2 $TEST_DEVICE:/soundbanks/H/63
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 output=$($ecli elektron-data-ls $TEST_DEVICE:/soundbanks/H)
 actual=$(echo "$output" | grep "^F  64")
 expected=$(get_sound_n_with_id 1 64)
-[ "$actual" != "$expected" ] && cleanupAndExitWithError
+[ "$actual" != "$expected" ] && cleanupAndExit 1
 actual=$(echo "$output" | grep "^F  63")
 expected=$(get_sound_n_with_id 2 63)
-[ "$actual" != "$expected" ] && cleanupAndExitWithError
+[ "$actual" != "$expected" ] && cleanupAndExit 1
 
 echo "Testing data move..."
 $ecli elektron-data-mv $TEST_DEVICE:/soundbanks/H/64 $TEST_DEVICE:/soundbanks/H/62
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 output=$($ecli elektron-data-ls $TEST_DEVICE:/soundbanks/H)
 actual=$(echo "$output" | grep "^F  62")
 expected=$(get_sound_n_with_id 1 62)
-[ "$actual" != "$expected" ] && cleanupAndExitWithError
+[ "$actual" != "$expected" ] && cleanupAndExit 1
 actual=$(echo "$output" | grep "^F  64")
-[ -n "$actual" ] && cleanupAndExitWithError
+[ -n "$actual" ] && cleanupAndExit 1
 
 echo "Testing data swap..."
 $ecli elektron-data-sw $TEST_DEVICE:/soundbanks/H/62 $TEST_DEVICE:/soundbanks/H/63
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 output=$($ecli elektron-data-ls $TEST_DEVICE:/soundbanks/H)
 actual=$(echo "$output" | grep "^F  62")
 expected=$(get_sound_n_with_id 2 62)
-[ "$actual" != "$expected" ] && cleanupAndExitWithError
+[ "$actual" != "$expected" ] && cleanupAndExit 1
 actual=$(echo "$output" | grep "^F  63")
 expected=$(get_sound_n_with_id 1 63)
-[ "$actual" != "$expected" ] && cleanupAndExitWithError
+[ "$actual" != "$expected" ] && cleanupAndExit 1
 
 echo "Testing data clear..."
 $ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/63
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 $ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/62
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 output=$($ecli elektron-data-ls $TEST_DEVICE:/soundbanks/H)
-[ $(echo "$output" | grep "^F  62" | wc -l) -ne 0 ] && cleanupAndExitWithError
-[ $(echo "$output" | grep "^F  63" | wc -l) -ne 0 ] && cleanupAndExitWithError
+[ $(echo "$output" | grep "^F  62" | wc -l) -ne 0 ] && cleanupAndExit 1
+[ $(echo "$output" | grep "^F  63" | wc -l) -ne 0 ] && cleanupAndExit 1
 
 echo "Testing upload..."
-$ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/1
 $ecli elektron-data-ul $srcdir/res/SOUND.dtdata $TEST_DEVICE:/soundbanks/H
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 id=$($ecli elektron-data-ls $TEST_DEVICE:/soundbanks/H | head -n 1 | grep 'SOUND$' | awk '{print $2}')
 
 echo "Testing download..."
 $ecli elektron-data-dl $TEST_DEVICE:/soundbanks/H/$id
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 ls "SOUND.dtdata"
 cksum SOUND.dtdata
 cksum $srcdir/res/SOUND.dtdata
 actual_cksum="$(cksum SOUND.dtdata | awk '{print $1}')"
-[ "$actual_cksum" != "$(cksum $srcdir/res/SOUND.dtdata | awk '{print $1}')" ] && cleanupAndExitWithError
+[ "$actual_cksum" != "$(cksum $srcdir/res/SOUND.dtdata | awk '{print $1}')" ] && cleanupAndExit 1
 rm SOUND.dtdata
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 $ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/$id
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 
 echo "Testing upload..."
 $ecli elektron-data-ul $srcdir/res/SOUND.dtdata $TEST_DEVICE:/soundbanks/H/256
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 id=$($ecli elektron-data-ls $TEST_DEVICE:/soundbanks/H | tail -n 1 | grep 'SOUND$' | awk '{print $2}')
-[ $id != 256 ] && cleanupAndExitWithError
+[ $id != 256 ] && cleanupAndExit 1
 
 echo "Testing data clear..."
 $ecli elektron-data-cl $TEST_DEVICE:/soundbanks/H/256
-[ $? -ne 0 ] && cleanupAndExitWithError
+[ $? -ne 0 ] && cleanupAndExit 1
 
-exit 0
+cleanupAndExit 0
