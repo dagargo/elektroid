900d4ab00110e3dfb85ebec737c4cc39ac162352
dagargo <dagargo@gmail.com>
2023-06-11 12:28:31 +0200

    Fix editor shortcut to save samples

--- src/editor.c
+++ src/editor.c
@@ -839,7 +839,7 @@ editor_key_press (GtkWidget * widget, GdkEventKey * event, gpointer data)
     {
       editor_delete_clicked (NULL, editor);
     }
-  else if (event->state & GDK_CONTROL_MASK && event->keyval == GDK_KEY_S)
+  else if (event->state & GDK_CONTROL_MASK && event->keyval == GDK_KEY_s)
     {
       editor_save_clicked (NULL, editor);
     }
