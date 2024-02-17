2dce95f6cfc332c86e2111ee0c2041b5e5a4e3f7
dagargo <dagargo@gmail.com>
2024-02-17 10:10:10 +0100

    Add support for Elektron Machinedrum

--- src/connectors/machinedrum.h
+++ src/connectors/machinedrum.h
@@ -1,5 +1,5 @@
 /*
- *   regconn.c
+ *   machinedrum.h
  *   Copyright (C) 2024 David García Goñi <dagargo@gmail.com>
  *
  *   This file is part of Elektroid.
@@ -18,33 +18,11 @@
  *   along with Elektroid. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "regconn.h"
+#ifndef MACHINEDRUM_H
+#define MACHINEDRUM_H
 
-#include "connectors/system.h"
-#include "connectors/elektron.h"
-#include "connectors/machinedrum.h"
-#include "connectors/microbrute.h"
-#include "connectors/microfreak.h"
-#include "connectors/cz.h"
-#include "connectors/sds.h"
-#include "connectors/efactor.h"
-#include "connectors/phatty.h"
-#include "connectors/summit.h"
-#include "connectors/default.h"
+#include "connector.h"
 
-void
-regconn_register ()
-{
-  system_connector = &CONNECTOR_SYSTEM;
-  gslist_fill (&connectors, &CONNECTOR_ELEKTRON, &CONNECTOR_MICROBRUTE,
-	       &CONNECTOR_MICROFREAK, &CONNECTOR_PHATTY, &CONNECTOR_SUMMIT,
-	       &CONNECTOR_CZ, &CONNECTOR_SDS, &CONNECTOR_EFACTOR,
-	       &CONNECTOR_DEFAULT, NULL);
-}
+extern const struct connector CONNECTOR_MACHINEDRUM;
 
-void
-regconn_unregister ()
-{
-  g_slist_free (connectors);
-  connectors = NULL;
-}
+#endif
