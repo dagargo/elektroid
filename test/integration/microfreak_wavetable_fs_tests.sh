225f29cf5cf7849bc9d5a05efcb5f5b3ff49692b
dagargo <dagargo@gmail.com>
2024-06-08 16:19:58 +0200

    Fix microfreak wavetable tests

--- test/integration/microfreak_wavetable_fs_tests.sh
+++ test/integration/microfreak_wavetable_fs_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-$srcdir/integration/generic_fs_tests.sh microfreak wavetable / 16 "/0 /17" /16 "Name" ""
+$srcdir/integration/generic_fs_tests.sh microfreak wavetable / 16 "/0 /17" /16 "Name" "New Name"
 
 exit $?
