b2944368d12b2946dd069c111be13a88ba368333
dagargo <dagargo@gmail.com>
2024-06-25 22:58:13 +0200

    Add and improve sample tests

--- test/tests_sample.c
+++ test/tests_sample.c
@@ -2,8 +2,8 @@
 #include <CUnit/Basic.h>
 #include "../src/sample.h"
 
-void
-test_load_sample (struct job_control *control)
+static void
+test_load_sample_resampling (struct job_control *control)
 {
   gint err;
   struct idata sample;
@@ -22,6 +22,10 @@ test_load_sample (struct job_control *control)
 			       control, &sample_info_req, &sample_info_src);
 
   CU_ASSERT_EQUAL (err, 0);
+  if (err)
+    {
+      return;
+    }
 
   CU_ASSERT_EQUAL (sample_info_src.frames, 44100);
   CU_ASSERT_EQUAL (sample_info_src.loop_start, 5817);
@@ -42,23 +46,96 @@ test_load_sample (struct job_control *control)
   CU_ASSERT_EQUAL (sample_info->channels, 1);
   CU_ASSERT_EQUAL (sample_info->midi_note, 0);
 
+  CU_ASSERT_EQUAL (sample.content->len, 48000 * 2);
+
+  CU_ASSERT_EQUAL (0, memcmp (sample.content->data,
+			      "\xa3\x03\x49\x4f\xeb\x6a\x51\x62", 8));
+
   idata_free (&sample);
 }
 
-void
-test_load_sample_control ()
+static void
+test_load_sample_control_resampling ()
 {
   struct job_control control;
   control.active = TRUE;
   control.callback = NULL;
   g_mutex_init (&control.mutex);
-  test_load_sample (&control);
+  test_load_sample_resampling (&control);
 }
 
-void
-test_load_sample_no_control ()
+static void
+test_load_sample_no_control_resampling ()
 {
-  test_load_sample (NULL);
+  test_load_sample_resampling (NULL);
+}
+
+static void
+test_load_sample_no_resampling (struct job_control *control)
+{
+  gint err;
+  struct idata sample;
+  struct sample_info sample_info_req;
+  struct sample_info sample_info_src;
+  struct sample_info *sample_info;
+
+  printf ("\n");
+
+  sample_info_req.channels = 1;
+  sample_info_req.rate = 48000;
+  sample_info_req.format = SF_FORMAT_PCM_16;
+
+  err = sample_load_from_file (TEST_DATA_DIR
+			       "/connectors/square-wav48k16b1c.wav", &sample,
+			       control, &sample_info_req, &sample_info_src);
+
+  CU_ASSERT_EQUAL (err, 0);
+  if (err)
+    {
+      return;
+    }
+
+  CU_ASSERT_EQUAL (sample_info_src.frames, 48000);
+  CU_ASSERT_EQUAL (sample_info_src.loop_start, 6331);
+  CU_ASSERT_EQUAL (sample_info_src.loop_end, 43312);
+  CU_ASSERT_EQUAL (sample_info_src.loop_type, 0x7f);
+  CU_ASSERT_EQUAL (sample_info_src.rate, 48000);
+  CU_ASSERT_EQUAL (sample_info_src.format, SF_FORMAT_WAV | SF_FORMAT_PCM_16);
+  CU_ASSERT_EQUAL (sample_info_src.channels, 1);
+  CU_ASSERT_EQUAL (sample_info_src.midi_note, 0);
+
+  sample_info = sample.info;
+  CU_ASSERT_EQUAL (sample_info->frames, 48000);
+  CU_ASSERT_EQUAL (sample_info->loop_start, 6331);
+  CU_ASSERT_EQUAL (sample_info->loop_end, 43312);
+  CU_ASSERT_EQUAL (sample_info->loop_type, 0x7f);
+  CU_ASSERT_EQUAL (sample_info->rate, 48000);
+  CU_ASSERT_EQUAL (sample_info->format, SF_FORMAT_PCM_16);
+  CU_ASSERT_EQUAL (sample_info->channels, 1);
+  CU_ASSERT_EQUAL (sample_info->midi_note, 0);
+
+  CU_ASSERT_EQUAL (sample.content->len, 48000 * 2);
+
+  CU_ASSERT_EQUAL (0, memcmp (sample.content->data,
+			      "\xff\xff\x8d\x53\xc8\x67\x1d\x66", 8));
+
+  idata_free (&sample);
+}
+
+static void
+test_load_sample_control_no_resampling ()
+{
+  struct job_control control;
+  control.active = TRUE;
+  control.callback = NULL;
+  g_mutex_init (&control.mutex);
+  test_load_sample_no_resampling (&control);
+}
+
+static void
+test_load_sample_no_control_no_resampling ()
+{
+  test_load_sample_no_resampling (NULL);
 }
 
 gint
@@ -78,13 +155,30 @@ main (gint argc, gchar *argv[])
       goto cleanup;
     }
 
-  if (!CU_add_test (suite, "load_sample_control", test_load_sample_control))
+  if (!CU_add_test
+      (suite, "load_sample_control_resampling",
+       test_load_sample_control_resampling))
     {
       goto cleanup;
     }
 
-  if (!CU_add_test (suite, "load_sample_no_control",
-		    test_load_sample_no_control))
+  if (!CU_add_test
+      (suite, "load_sample_no_control_resampling",
+       test_load_sample_no_control_resampling))
+    {
+      goto cleanup;
+    }
+
+  if (!CU_add_test
+      (suite, "load_sample_control_no_resampling",
+       test_load_sample_control_no_resampling))
+    {
+      goto cleanup;
+    }
+
+  if (!CU_add_test
+      (suite, "load_sample_no_control_no_resampling",
+       test_load_sample_no_control_no_resampling))
     {
       goto cleanup;
     }
