dagargo/elektroid

commit 5ea13e6ad5e3cac62eadc18b01dbab90a4ca237c

Allow having raw set to NULL in backend_tx_and_rx_sysex_transfer

src/backend.c
@@ -190,11 +190,14 @@ backend_tx_and_rx_sysex_transfer (struct backend *backend,
 
   g_mutex_lock (&backend->mutex);
 
-  backend_tx_sysex (backend, transfer);
-  if (free)
+  if (transfer->raw)
     {
-      free_msg (transfer->raw);
-      transfer->raw = NULL;
+      backend_tx_sysex (backend, transfer);
+      if (free)
+	{
+	  free_msg (transfer->raw);
+	  transfer->raw = NULL;
+	}
     }
   if (!transfer->err)
     {

src/connectors/microfreak.c
@@ -1002,8 +1002,7 @@ microfreak_sample_upload_tx_and_rx (struct backend *backend,
       while (seq != (*rx_msg)->data[sizeof (MICROFREAK_REQUEST_HEADER)])
 	{
 	  free_msg (*rx_msg);
-	  GByteArray *aux = g_byte_array_new ();	//This is an empty message
-	  *rx_msg = backend_tx_and_rx_sysex (backend, aux, -1);
+	  *rx_msg = backend_tx_and_rx_sysex (backend, NULL, -1);
 	  if (!*rx_msg)
 	    {
 	      err = -EIO;
@@ -1113,16 +1112,14 @@ microfreak_sample_upload (struct backend *backend, const gchar *path,
   free_msg (rx_msg);
   if (err)
     {
-      tx_msg = g_byte_array_new ();	//This is an empty message
-      common_data_tx_and_rx_part (backend, tx_msg, &rx_msg, control);
+      common_data_tx_and_rx_part (backend, NULL, &rx_msg, control);
       free_msg (rx_msg);
       goto end;
     }
 
   usleep (MICROFREAK_REST_TIME_US);
 
-  tx_msg = g_byte_array_new ();	//This is an empty message
-  err = common_data_tx_and_rx_part (backend, tx_msg, &rx_msg, control);
+  err = common_data_tx_and_rx_part (backend, NULL, &rx_msg, control);
   if (err)
     {
       goto end;

commit 8deb25835ca3a7763bf49033d4c56237bbe8ceac

microfreak: Fix random error when uploading samples
In case there is no data to be sent, the member err in the struct transfer is
uninitialized, which leads to random behaviour as reception could be skipped.

The bug was introduced in 5ea13e6ad5e3cac62eadc18b01dbab90a4ca237c.

src/backend.c
@@ -188,6 +188,7 @@ backend_tx_and_rx_sysex_transfer (struct backend *backend,
 				  gboolean free)
 {
   transfer->batch = FALSE;
+  transfer->err = 0;
 
   g_mutex_lock (&backend->mutex);
 
@@ -200,6 +201,7 @@ backend_tx_and_rx_sysex_transfer (struct backend *backend,
 	  transfer->raw = NULL;
 	}
     }
+
   if (!transfer->err)
     {
       backend_rx_sysex (backend, transfer);