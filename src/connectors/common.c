dagargo/elektroid

commit fcca4d654f83e4f0f84d1c47cd062f982256dc5f

Change get_exts function signature in fs_operations struct
This is a preparation for reading the Elektron filesystem extensions from the JSON
file. As this is a quite complicated refactor, changes in the application and all the
connectors except the Elektron one, are done in this commit.
All Elektron connectors have been disabled in this commit and will be enabled again
in the next commit.

src/connectors/common.c
@@ -22,6 +22,14 @@
 #include "common.h"
 #include "sample.h"
 
+static const gchar *SYSEX_EXTS[] = { "syx", NULL };
+
+const gchar **
+common_sysex_get_extensions ()
+{
+  return SYSEX_EXTS;
+}
+
 gchar *
 common_slot_get_upload_path (struct backend *backend,
 			     const struct fs_operations *ops,
@@ -223,6 +231,7 @@ common_slot_get_download_path_id_name (struct backend *backend,
 				       const gchar *name)
 {
   gchar *path;
+  const gchar *ext = GET_SAVE_EXT (ops, backend);
   GString *str = g_string_new (NULL);
 
   g_string_append_printf (str, "%s %s", backend->name, ops->name);
@@ -239,7 +248,7 @@ common_slot_get_download_path_id_name (struct backend *backend,
     }
 
   g_string_append (str, ".");
-  g_string_append (str, ops->ext);
+  g_string_append (str, ext);
 
   path = path_chain (PATH_SYSTEM, dst_dir, str->str);
   g_string_free (str, TRUE);
@@ -345,8 +354,9 @@ common_system_get_download_path (struct backend *backend,
 				 const gchar *dst_dir, const gchar *src_path,
 				 struct idata *idata)
 {
+  const gchar *ext = GET_SAVE_EXT (ops, backend);
   GString *name_with_ext = g_string_new (NULL);
-  g_string_append_printf (name_with_ext, "%s.%s", idata->name, ops->ext);
+  g_string_append_printf (name_with_ext, "%s.%s", idata->name, ext);
   gchar *path = path_chain (PATH_SYSTEM, dst_dir, name_with_ext->str);
   g_string_free (name_with_ext, TRUE);
   return path;

src/connectors/common.h
@@ -32,6 +32,8 @@ struct common_simple_read_dir_data
   guint32 last;
 };
 
+const gchar **common_sysex_get_extensions ();
+
 gchar *common_slot_get_upload_path (struct backend *backend,
 				    const struct fs_operations *ops,
 				    const gchar * dst_dir,

src/connectors/default.c
@@ -50,7 +50,7 @@ default_next_dentry (struct item_iterator *iter)
 
 static gint
 default_read_dir (struct backend *backend, struct item_iterator *iter,
-		  const gchar *dir, GSList *extensions)
+		  const gchar *dir, const gchar **extensions)
 {
   if (strcmp (dir, "/"))
     {

src/Makefile.am
@@ -53,8 +53,6 @@ utils.c utils.h \
 backend.c backend.h $(elektroid_backend_sources) \
 connectors/common.c connectors/common.h \
 connectors/system.c connectors/system.h \
-connectors/elektron.c connectors/elektron.h \
-connectors/package.c connectors/package.h \
 connectors/microbrute.c connectors/microbrute.h \
 connectors/microfreak_sample.c connectors/microfreak_sample.h \
 connectors/microfreak.c connectors/microfreak.h \

src/browser.c
@@ -563,7 +563,7 @@ browser_iterate_dir (struct browser *browser, struct item_iterator *iter,
 static void
 browser_iterate_dir_recursive (struct browser *browser, const gchar *rel_dir,
 			       struct item_iterator *iter, const gchar *icon,
-			       GSList *extensions)
+			       const gchar **extensions)
 {
   gint err;
   gchar *child_dir, *child_rel_dir;
@@ -599,65 +599,36 @@ browser_iterate_dir_recursive (struct browser *browser, const gchar *rel_dir,
     }
 }
 
-static GSList *
-browser_get_remote_browser_exts ()
-{
-  GSList *exts;
-  if (remote_browser.fs_ops->get_exts)
-    {
-      exts = remote_browser.fs_ops->get_exts (remote_browser.backend,
-					      remote_browser.fs_ops);
-    }
-  else
-    {
-      if (remote_browser.fs_ops->ext)
-	{
-	  exts = g_slist_append (NULL, strdup (remote_browser.fs_ops->ext));
-	}
-      else
-	{
-	  exts = NULL;
-	}
-    }
-  return exts;
-}
-
-static const gchar *
-browser_get_icon_for_local_browser ()
-{
-  const gchar *icon;
-  if (remote_browser.fs_ops->upload)
-    {
-      icon = remote_browser.fs_ops->gui_icon;
-    }
-  else
-    {
-      icon = local_browser.fs_ops->gui_icon;
-    }
-  return icon;
-}
-
 static gpointer
 browser_load_dir_runner (gpointer data)
 {
   gint err;
   struct browser *browser = data;
   struct item_iterator iter;
-  GSList *exts;
+  const gchar **exts;
   const gchar *icon;
   gboolean search_mode;
 
   if (browser == &remote_browser)
     {
-      exts = browser_get_remote_browser_exts (remote_browser.fs_ops->ext);
+      exts = remote_browser.fs_ops->get_exts (remote_browser.backend,
+					      remote_browser.fs_ops);
       icon = remote_browser.fs_ops->gui_icon;
     }
   else
     {
       if (remote_browser.fs_ops)
 	{
-	  exts = browser_get_remote_browser_exts ();
-	  icon = browser_get_icon_for_local_browser ();
+	  exts = remote_browser.fs_ops->get_exts (remote_browser.backend,
+						  remote_browser.fs_ops);
+	  if (remote_browser.fs_ops->upload)
+	    {
+	      icon = remote_browser.fs_ops->gui_icon;
+	    }
+	  else
+	    {
+	      icon = local_browser.fs_ops->gui_icon;
+	    }
 	}
       else
 	{
@@ -694,7 +665,6 @@ browser_load_dir_runner (gpointer data)
 
 end:
   g_idle_add (browser_load_dir_runner_update_ui, browser);
-  g_slist_free_full (exts, g_free);
   return NULL;
 }
 

src/connector.c
@@ -48,7 +48,7 @@ item_iterator_free (struct item_iterator *iter)
 
 gboolean
 item_iterator_is_dir_or_matches_extensions (struct item_iterator *iter,
-					    const GSList *extensions)
+					    const gchar **extensions)
 {
   if (iter->item.type == ITEM_TYPE_DIR)
     {

src/connector.h
@@ -31,6 +31,8 @@
 #define FS_ICON_GENERIC "elektroid-file-symbolic"
 #define FS_ICON_KEYS "elektroid-keys-symbolic"
 
+#define GET_SAVE_EXT(ops,backend) (ops->get_exts(backend, ops)[0])
+
 enum item_type
 {
   ITEM_TYPE_NONE = 0,
@@ -75,8 +77,9 @@ struct fs_operations;
 typedef void (*fs_print_item) (struct item_iterator *, struct backend *,
 			       const struct fs_operations *);
 
-typedef gint (*fs_init_iter_func) (struct backend *, struct item_iterator *,
-				   const gchar *, GSList *);
+typedef gint (*fs_init_iter_func) (struct backend * backend,
+				   struct item_iterator * iter,
+				   const gchar * dir, const gchar ** exts);
 
 typedef gint (*fs_path_func) (struct backend *, const gchar *);
 
@@ -91,8 +94,8 @@ typedef gchar *(*fs_get_item_slot) (struct item *, struct backend *);
 typedef gint (*fs_local_file_op) (const gchar *, struct idata *,
 				  struct job_control *);
 
-typedef GSList *(*fs_get_exts) (struct backend *,
-				const struct fs_operations *);
+typedef const gchar **(*fs_get_exts) (struct backend *,
+				      const struct fs_operations *);
 
 typedef gchar *(*fs_get_path) (struct backend * backend,
 			       const struct fs_operations * ops,
@@ -118,7 +121,6 @@ struct fs_operations
   const gchar *name;
   const gchar *gui_name;
   const gchar *gui_icon;
-  const gchar *ext;
   guint32 max_name_len;
   fs_init_iter_func readdir;	//This function runs on its own thread so it can take as long as needed in order to make calls to item_iterator_next not to wait for IO.
   fs_file_exists file_exists;
@@ -135,7 +137,7 @@ struct fs_operations
   fs_local_file_op save;	//Write a file from memory to the OS storage. Typically used after download.
   fs_local_file_op load;	//Load a file from the OS storage into memory. Typically used before upload.
   fs_get_item_slot get_slot;	//Optionally used by slot filesystems to show a custom slot name column such `A01` or `[P-01]`. Needs FS_OPTION_SHOW_SLOT_COLUMN.
-  fs_get_exts get_exts;		//If present, this is used; if not, type_ext is used.
+  fs_get_exts get_exts;		//Length must be one at least. First element will be used as file extension and all will be used as loading extensions.
   fs_get_path get_upload_path;
   fs_get_path get_download_path;
   fs_select_item select_item;
@@ -184,7 +186,7 @@ void item_iterator_free (struct item_iterator *iter);
 
 gboolean item_iterator_is_dir_or_matches_extensions (struct item_iterator
 						     *iter,
-						     const GSList *
+						     const gchar **
 						     extensions);
 
 /**

src/connectors/cz.c
@@ -155,7 +155,7 @@ get_mem_type (const gchar *name)
 
 static gint
 cz_read_dir (struct backend *backend, struct item_iterator *iter,
-	     const gchar *dir, GSList *extensions)
+	     const gchar *dir, const gchar **extensions)
 {
   gint mem_type;
 
@@ -299,13 +299,13 @@ static const struct fs_operations FS_PROGRAM_CZ_OPERATIONS = {
   .name = "program",
   .gui_name = "Programs",
   .gui_icon = FS_ICON_SND,
-  .ext = BE_SYSEX_EXT,
   .readdir = cz_read_dir,
   .print_item = common_print_item,
   .download = cz_download,
   .upload = cz_upload,
   .load = file_load,
   .save = file_save,
+  .get_exts = common_sysex_get_extensions,
   .get_upload_path = common_slot_get_upload_path,
   .get_download_path = cz_get_download_path,
   .select_item = common_midi_program_change

commit 0bf077e6822a2ab91c44f7b118418c684d6275d9

Fix crash when selecting a generic MIDI device
Bug introduced in fcca4d654f83e4f0f84d1c47cd062f982256dc5f.

src/connectors/common.c
@@ -52,6 +52,12 @@ common_sysex_get_extensions ()
   return SYSEX_EXTS;
 }
 
+const gchar **
+common_get_all_extensions ()
+{
+  return NULL;
+}
+
 gchar *
 common_slot_get_upload_path (struct backend *backend,
 			     const struct fs_operations *ops,

src/connectors/common.h
@@ -34,6 +34,8 @@ struct common_simple_read_dir_data
 
 const gchar **common_sysex_get_extensions ();
 
+const gchar **common_get_all_extensions ();
+
 gchar *common_slot_get_upload_path (struct backend *backend,
 				    const struct fs_operations *ops,
 				    const gchar * dst_dir,

src/connectors/default.c
@@ -73,7 +73,8 @@ const struct fs_operations FS_PROGRAM_DEFAULT_OPERATIONS = {
   .gui_icon = FS_ICON_SND,
   .readdir = default_read_dir,
   .print_item = common_print_item,
-  .select_item = common_midi_program_change
+  .select_item = common_midi_program_change,
+  .get_exts = common_get_all_extensions
 };
 
 static gint