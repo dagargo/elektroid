dagargo/elektroid

commit 2eb3265c25ab398ec33bfa3d5251a0d857c8f400

elektron: Fix RAM and tracks filesystem cases when path is NULL

src/connectors/elektron.c
@@ -3048,19 +3048,31 @@ elektron_ram_slot_next_entry (struct item_iterator *iter)
 
   iter->item.id = id;
   iter->item.type = ITEM_TYPE_FILE;
-  iter->item.size = size;
   item_set_object_info (&iter->item, "%s",
 			used ? ELEKTRON_RAM_SLOT_USED : "");
   if (size == 0)
     {
-      elektron_item_set_name (&iter->item, "");
+      item_set_name (&iter->item, "");
+      iter->item.size = -1;
+      iter->item.type = ITEM_TYPE_NONE;
     }
   else
     {
       gchar *path_cp1252;
       path_cp1252 = elektron_get_sample_path_from_hash_size (data->backend,
 							     hash, size);
-      elektron_item_set_name (&iter->item, path_cp1252);
+      if (path_cp1252)
+	{
+	  elektron_item_set_name (&iter->item, path_cp1252);
+	  iter->item.size = size;
+	  iter->item.type = ITEM_TYPE_FILE;
+	}
+      else
+	{
+	  item_set_name (&iter->item, "");
+	  iter->item.size = -1;
+	  iter->item.type = ITEM_TYPE_NONE;
+	}
     }
 
   return 0;
@@ -3326,8 +3338,16 @@ elektron_ram_track_next_entry (struct item_iterator *iter)
       gchar *path_utf8;
       path_cp1252 = elektron_get_sample_path_from_hash_size (data->backend,
 							     hash, size);
-      path_utf8 = elektron_name_to_utf8 (path_cp1252);
-      item_set_object_info (&iter->item, "slot=%d:path=%s", slot, path_utf8);
+      if (path_cp1252)
+	{
+	  path_utf8 = elektron_name_to_utf8 (path_cp1252);
+	}
+      else
+	{
+	  path_utf8 = NULL;
+	}
+      item_set_object_info (&iter->item, "slot=%d:path=%s", slot,
+			    path_utf8 ? path_utf8 : "");
       g_free (path_cp1252);
       g_free (path_utf8);
     }

commit 6b9ef9f0ad8d21f545157cde3ad9777e32515981

elektron: Fix upload to RAM
Empty slots were not detected as such and thus returning a ENOMEM.

The issue was introduced in 2eb3265c25ab398ec33bfa3d5251a0d857c8f400
because the way to detect empty slots changed. Now, an empty slot is
reported as having a size of -1, which not only is the standard way
but it also indicates that the size empty in the GUI and CLI.

src/connectors/elektron.c
@@ -3055,7 +3055,7 @@ elektron_ram_slot_next_entry (struct item_iterator *iter)
     {
       item_set_name (&iter->item, "");
       iter->item.size = -1;
-      iter->item.type = ITEM_TYPE_NONE;
+      iter->item.type = ITEM_TYPE_FILE;
     }
   else
     {
@@ -3072,7 +3072,7 @@ elektron_ram_slot_next_entry (struct item_iterator *iter)
 	{
 	  item_set_name (&iter->item, "");
 	  iter->item.size = -1;
-	  iter->item.type = ITEM_TYPE_NONE;
+	  iter->item.type = ITEM_TYPE_FILE;
 	}
     }
 
@@ -3420,7 +3420,7 @@ elektron_ram_get_free_slot (struct backend *backend)
 
   while (!item_iterator_next (&iter))
     {
-      if (iter.item.size == 0)
+      if (iter.item.size == -1)
 	{
 	  free_slot = iter.item.id;
 	  break;